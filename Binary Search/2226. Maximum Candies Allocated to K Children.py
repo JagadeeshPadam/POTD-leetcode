30366caa01be47c332a5eccd19a81b1559e40607
Jagadeesh Padam <jagadeeshpadam2003@gmail.com>
2025-03-14 22:02:11 +0530

    added 2226

--- Binary Search/2226. Maximum Candies Allocated to K Children.py
+++ Binary Search/2226. Maximum Candies Allocated to K Children.py
@@ -1,10 +1,17 @@
 class Solution:
-    def maximumCandies(self, A, k):
-        left, right = 0, sum(A) / k
-        while left < right:
-            mid = (left + right + 1) / 2
-            if k > sum(a / mid for a in A):
-                right = mid - 1
+    def maximumCandies(self, candies: List[int], k: int) -> int:        
+        left, right = 1, max(candies)
+        result = 0
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            children_count = sum(pile // mid for pile in candies)
+            
+            if children_count >= k:
+                result = mid
+                left = mid + 1
             else:
-                left = mid
-        return left 
+                right = mid - 1
+                
+        return result
